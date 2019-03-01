fd523410a5d5f982ac099d7d06b8edd5ec136486
ZWF <deathid@icloud.com>
2019-03-01 13:11:59 +0800

    python3中print函数中的参数end默认值为'\n'，表示换行，给end赋值为空，就不会换行了

--- 笨办法学python.py
+++ 笨办法学python.py
@@ -6,4 +6,5 @@
 # 6 print "I'd much rather you 'not'."
 # 7 print 'I "said" do not touch this.'
 
-print("Hello World !")
+print("Hello World !",end='')  #python3中print函数中的参数end默认值为'\n'，表示换行，给end赋值为空，就不会换行了
+print("Hello World !!\n")
